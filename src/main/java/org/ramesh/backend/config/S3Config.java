6c724b37cd70ffe39f42aaa416b805ee6840539e
FalconSlayer51 <dhanrajramesh22@gmail.com>
2026-01-12 23:06:06 +0530

    feat: changed few env vars

--- src/main/java/org/ramesh/backend/config/S3Config.java
+++ src/main/java/org/ramesh/backend/config/S3Config.java
@@ -3,19 +3,37 @@ package org.ramesh.backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+
     @Bean
     public S3Client s3Client(
             @Value("${aws.region}") String region
     ) {
+        var creds = buildCredentialsProvider();
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(creds)
                 .build();
+    }
+
+    private AwsCredentialsProvider buildCredentialsProvider() {
+        if (accessKey != null && !accessKey.isBlank() && secretKey != null && !secretKey.isBlank()) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey.trim(), secretKey.trim()));
+        }
+        return DefaultCredentialsProvider.create();
     }
 }
